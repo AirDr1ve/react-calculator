30bdc93eb822012cf54aab26d9195eeb5b7a3ecf
AirDr1ve <62071976+AirDr1ve@users.noreply.github.com>
2020-10-29 17:40:05 +0300

    v1.2

--- src/components/DeleteSymbolButton.js
+++ src/components/DeleteSymbolButton.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './DeleteSymbolButton.css';
 
  export const DeleteSymbolButton = (props) => (
-
             <div className='delete-btn' onClick={props.handleClick}>
                 {props.children} 
             </div>
-    
  )
 
